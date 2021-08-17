f2ff6c37901e8ad4f5ac1c27db5cd1af829d4d90
uyarber <86857361+uyarber@users.noreply.github.com>
2021-08-17 14:11:42 -0400

    Delete module3-solution directory

--- module3-solution/js/popper.js
+++
@@ -1,6 +0,0 @@
-import { createPopper } from '@popperjs/core';
-const popcorn = document.querySelector('#popcorn');
-const tooltip = document.querySelector('#tooltip');
-createPopper(popcorn, tooltip, {
-  placement: 'top',
-});
